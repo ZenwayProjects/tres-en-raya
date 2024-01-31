422b90accc6a400fe41a90048029af1b4a5f1f13
Zenway <zenway15@gmail.com>
2024-01-30 21:46:27 -0500

    feat: codigo que reconoce el ganador al momento del click pero muestra mensaje antes de que se renderice en pantalla la ultima jugada

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+import './App.css'
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+
+  const className=`square ${isSelected ? 'is-selected' : ''}`;
+
+  const handleClick = () => {
+    updateBoard(index);
+  }
+
+  return(
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+} 
+
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+  
+function App() {
+
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  
+  );
+  const [turn, setTurn] = useState(TURNS.X);
+  //null = sin ganador, false es empate
+  const [winner, setWinner] = useState(null) 
+
+  const checkWinner = (boardToCheck) => {
+    //Acá se revisa si hay un ganador
+    for(const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+
+    }
+    return null;
+  }
+  
+  const updateBoard = (index) => {
+    //si la casilla tiene marcado algo no se actualiza
+    if(board[index] || winner) return
+
+    //actualizar tablero
+    const newBoard = [... board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    //cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn)
+
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+      alert(`El ganador es ${newWinner}`)
+
+    }
+  }
+
+  return (
+    <main className='board'>
+      <h1>Tres en raya</h1>
+      <section className="game">
+        {board.map((_,index) =>{
+          return(
+           <Square 
+           key={index}
+           index={index}
+           updateBoard={updateBoard}
+           >
+            {board[index]}
+           </Square>
+          )
+          
+
+        })}
+      </section>
+      <section className='turn'>
+      <Square isSelected={turn===TURNS.X}>
+      {TURNS.X}
+      </Square>
+
+      <Square isSelected={turn===TURNS.O}>
+      {TURNS.O}
+      </Square>
+      </section>
+    </main>
+  
+  )
+}
+
+export default App
